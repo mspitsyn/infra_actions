c58323e9e4ff8e80744cd40ca00144ead9ad3ef2
Максим Спицын <mgraf@yandex.ru>
2022-08-30 16:55:37 +0300

    тест запуска 16

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось обновить проект и автоматически его запустить на сервере!!!')
 
 
 def second_page(request):
